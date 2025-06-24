8cab57e30a5e6eebbfd4bd48ca32f3c45b376949
HUAXIANs <yangye1255799207@gmail.com>
2025-06-25 01:30:39 +0800

    再次尝试适配vercel的 in18

--- nexaura-frontend/vite.config.js
+++ nexaura-frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js',
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
